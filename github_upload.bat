1b3b533f371b61ec29c1f3f512ad8d246263f61c
rajsubhajit554-cloud <rajsubhajit554-cloud@users.noreply.github.com>
2026-06-07 13:13:32 +0530

    Update text, video, and looping behavior

--- github_upload.bat
+++ github_upload.bat
@@ -8,8 +8,9 @@ echo Adding all changed files to Git...
 git add .
 
 echo.
+set commitMsg=
 set /p commitMsg="Enter commit message (Press ENTER for default 'Website update'): "
-if "%commitMsg%"=="" set commitMsg="Website update"
+if "%commitMsg%"=="" set commitMsg=Website update
 
 echo.
 echo Committing changes...
